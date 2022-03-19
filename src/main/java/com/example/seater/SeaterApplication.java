9919ea89bd7fda32a51de78d9782ef8a3c3322da
Filip <filip.pranklin@gmail.com>
2022-03-19 12:05:51 +0100

    Inital commit

---
+++ src/main/java/com/example/seater/SeaterApplication.java
@@ -0,0 +1,13 @@
+package com.example.seater;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeaterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeaterApplication.class, args);
+	}
+
+}
