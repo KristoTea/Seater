aec006a374a2d7868299c15602a3ba5256f5a898
Marta <dulibicmarta00@gmail.com>
2022-03-19 12:17:57 +0100

    Add backend project

---
+++ seater_backend/src/main/java/com/example/seater_backend/SeaterBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.seater_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeaterBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SeaterBackendApplication.class, args);
+    }
+
+}
---
+++ seater_backend/src/test/java/com/example/seater_backend/SeaterBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.seater_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeaterBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
