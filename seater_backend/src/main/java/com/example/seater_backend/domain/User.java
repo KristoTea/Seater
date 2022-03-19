cfa70111e98dc5158b4b6f35065324dba041bd69
Marta <dulibicmarta00@gmail.com>
2022-03-19 13:34:58 +0100

    Add database credentials

--- seater_backend/src/main/java/com/example/seater_backend/domain/User.java
+++ seater_backend/src/main/java/com/example/seater_backend/domain/User.java
@@ -16,8 +16,52 @@ public class User {
 
     private String surname;
 
+    private String position;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
     @Column(unique=true, nullable=false)
     private String username;
 
     private String password;
+
+
 }
