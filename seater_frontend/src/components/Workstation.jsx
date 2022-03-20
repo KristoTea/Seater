09ba990761a69974f5caa62c4c66683e51393301
Lucija Aleksić <lucija.aleksic@fer.hr>
2022-03-20 03:20:04 +0100

    fixes

--- seater_frontend/src/components/Workstation.jsx
+++ seater_frontend/src/components/Workstation.jsx
@@ -1,19 +1,21 @@
 import React from "react";
 
 import WorkstationChip from "../components/WorkstationChip";
+import { Button } from "@mui/material";
 
-export default function Workstation({ equipment }) {
+export default function Workstation({ equipment, assignee }) {
   // console.log(equipment)
   const e = equipment.split(", ");
+  const isAdmin = localStorage.getItem("user").username === "admin";
   return (
     <div
       style={{
         width: "45%",
         marginBottom: 10,
         minHeight: "150px",
-        paddingTop: "2rem",
-        background: "#98bdb8",
+        background: "#4e8f7b",
         padding: "10px",
+        paddingTop: "25px",
         borderRadius: 5,
         position: "relative",
       }}
@@ -25,14 +27,37 @@ export default function Workstation({ equipment }) {
           left: "43%",
           width: "20px",
           height: "20px",
-          background: "#98bdb8",
+          background: "#7ab097",
           borderRadius: "20px",
           border: "5px solid #414141",
         }}
       />
+      <div
+        style={{
+          position: "absolute",
+          top: -5,
+          left: "53%",
+          fontWeight: 700,
+          textShadow: `-1px -1px 0 white, 
+          1px -1px 0 white, -1px 1px 0 white, 1px 1px 0 white`,
+        }}
+      >
+        assignee
+      </div>
       {e.map((eq) => (
         <WorkstationChip type={eq} />
       ))}
+      {isAdmin && (
+        <Button
+          style={{
+            position: "absolute",
+            bottom: 0,
+            right: 0,
+          }}
+        >
+          Assign
+        </Button>
+      )}
     </div>
   );
 }
